5f9d22283174a351c7dfb801b8415d58921855c3
JohannesHoppe <johannes.hoppe@haushoppe-its.de>
2015-11-26 09:51:55 +0100

    first test of component -- module loader needs to be fixed!

--- src/test/components/dashboard/dashboard.component_test.ts
+++ src/test/components/dashboard/dashboard.component_test.ts
@@ -1,21 +1,14 @@
 import { iit, it, ddescribe, describe, expect, inject, beforeEachProviders, } from 'angular2/testing';
 //import { injectAsync, fakeAsync, tick } from 'angular2/testing';
 import { provide } from 'angular2/angular2';
-/*
-import { UserService } from '../app/user-service';
+
+import DashboardComponent from '../../../app/components/dashboard/dashboard.component';
+import GasService from '../../../app/models/gasService';
 
 describe('dashboard component', () => {
-    beforeEachProviders(() => [ provide(LoginService, {useClass: MockLoginService}), UserService
-  ]);
+  beforeEachProviders(() => [DashboardComponent, GasService]);
 
-  it('should ask for PIN', injectAsync([TestComponentBuilder], (tcb) => {
-    return tcb.createAsync(GreetingComponent).then((fixture) => {
-      fixture.detectChanges();
-      var compiled = fixture.debugElement.nativeElement;
-
-
-      expect(compiled).toContainText('Enter PIN');
-      expect(compiled.querySelector('h3')).toHaveText('Status: Enter PIN');
-    });
-  }));
-*/
+    it('should have a predefined list of cars', inject([DashboardComponent], (dashboard: DashboardComponent) => {
+      expect(dashboard.cars.length).toBe(2);
+    }));
+});
