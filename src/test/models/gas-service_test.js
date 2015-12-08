7f41e5eb3eb54c90e4cbfb13437f2f96c01d2669
JohannesHoppe <johannes.hoppe@haushoppe-its.de>
2015-12-08 08:33:24 +0100

    i hereby present: an async test with MockBackend :star:

--- src/test/models/gas-service_test.js
+++ src/test/models/gas-service_test.js
@@ -1,5 +1,6 @@
 var testing_1 = require('angular2/testing');
 var angular2_1 = require('angular2/angular2');
+var mock_backend_1 = require('angular2/src/http/backends/mock_backend');
 var http_1 = require('angular2/http');
 var gas_service_1 = require('../../app/models/gas-service');
 testing_1.describe('GasService', function () {
@@ -7,20 +8,19 @@ testing_1.describe('GasService', function () {
     var httpMock = { useFactory: function (backend, defaultOptions) {
             return new http_1.Http(backend, defaultOptions);
         },
-        deps: [http_1.MockBackend, http_1.BaseRequestOptions] };
+        deps: [mock_backend_1.MockBackend, http_1.BaseRequestOptions] };
     testing_1.beforeEachProviders(function () { return [
-        http_1.BaseRequestOptions, http_1.MockBackend,
+        http_1.BaseRequestOptions, mock_backend_1.MockBackend,
         angular2_1.provide(http_1.Http, httpMock),
         gas_service_1.default
     ]; });
-    testing_1.it('should pick up the first price', testing_1.inject([gas_service_1.default, http_1.MockBackend], function (gasService, backend) {
-        debugger;
-        var connection;
-        backend.connections.subscribe(function (c) { return connection = c; });
-        connection.mockRespond(new http_1.Response('fake response'));
-        gasService.getBestPrice().subscribe(function (res) {
-            debugger;
-            testing_1.expect(res.text()).toBe('awesome');
+    testing_1.it('should pick up the first price', testing_1.inject([gas_service_1.default, mock_backend_1.MockBackend], function (gasService, backend) {
+        var fakeResponse = JSON.stringify({ stations: [{ price: 42 }, { price: 4 }] });
+        backend.connections.subscribe(function (connection) {
+            connection.mockRespond(new http_1.Response(new http_1.ResponseOptions({ body: fakeResponse })));
+        });
+        gasService.getBestPrice().subscribe(function (price) {
+            testing_1.expect(price).toBe(42);
         });
     }));
 });
--- src/test/models/gas-service_test.ts
+++ src/test/models/gas-service_test.ts
@@ -1,6 +1,23 @@
 import { beforeEachProviders, describe, expect, iit, inject, it, injectAsync, fakeAsync, TestComponentBuilder, tick } from 'angular2/testing';
 import { Component, provide} from 'angular2/angular2';
-import {MockBackend, BaseRequestOptions, Http, Response } from 'angular2/http';
+//import {MockBackend, BaseRequestOptions, Http, Response } from 'angular2/http';
+
+import {MockBackend, MockConnection} from 'angular2/src/http/backends/mock_backend';
+import {
+  BaseRequestOptions,
+  ConnectionBackend,
+  Request,
+    RequestOptions,
+  Response,
+  ResponseOptions,
+  URLSearchParams,
+  JSONP_PROVIDERS,
+  HTTP_PROVIDERS,
+  XHRBackend,
+  JSONPBackend,
+  Http,
+  Jsonp
+} from 'angular2/http';
 
 import GasService from '../../app/models/gas-service';
 
@@ -9,7 +26,7 @@ describe('GasService', () => {
 
     var connection;
     var httpMock = { useFactory:
-        function(backend, defaultOptions) {
+        function(backend: ConnectionBackend, defaultOptions: BaseRequestOptions) {
             return new Http(backend, defaultOptions);
         },
         deps: [MockBackend, BaseRequestOptions]};
@@ -23,15 +40,14 @@ describe('GasService', () => {
     // this is an async test!
     it('should pick up the first price', inject([GasService, MockBackend], (gasService, backend) => {
 
-      debugger;
-      var connection;
-      backend.connections.subscribe(c => connection = c);
-      connection.mockRespond(new Response(<any>'fake response'));
+      var fakeResponse = JSON.stringify({ stations: [{ price: 42 }, { price: 4 }] });
 
-      gasService.getBestPrice().subscribe((res) => {
-        debugger;
-        expect(res.text()).toBe('awesome');
+      backend.connections.subscribe(connection => {
+         connection.mockRespond(new Response(new ResponseOptions({body: fakeResponse})));
+       });
+
+      gasService.getBestPrice().subscribe((price) => {
+        expect(price).toBe(42);
       });
     }));
-
 });
