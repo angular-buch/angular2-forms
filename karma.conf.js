43bc6646911b3e7c5f84fc5e6bf133475496c97f
Gregor Woiwode <gw@co-it.eu>
2015-12-29 15:23:44 +0100

    References new angular dependencies to run tests

--- karma.conf.js
+++ karma.conf.js
@@ -7,10 +7,12 @@ module.exports = function(config) {
 
     files: [
       // paths loaded by Karma
+      {pattern: 'node_modules/angular2/bundles/angular2-polyfills.js', included: true, watched: true},
       {pattern: 'node_modules/systemjs/dist/system.src.js', included: true, watched: true},
-      {pattern: 'node_modules/angular2/bundles/angular2.js', included: true, watched: true},
-      {pattern: 'node_modules/angular2/bundles/http.js', included: true, watched: true}, //  NEW
-      {pattern: 'node_modules/angular2/bundles/testing.js', included: true, watched: true},
+      {pattern: 'node_modules/angular2/bundles/angular2.dev.js', included: true, watched: true},
+      {pattern: 'node_modules/rxjs/bundles/Rx.js', included: true, watched: true},
+      {pattern: 'node_modules/angular2/bundles/http.dev.js', included: true, watched: true}, //  NEW
+      {pattern: 'node_modules/angular2/bundles/testing.dev.js', included: true, watched: true},
       {pattern: 'karma-test-shim.js', included: true, watched: true},
       //{pattern: 'src/test/matchers.js', included: true, watched: true},
 
